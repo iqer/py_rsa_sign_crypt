1a692294442d24a9d544bd460c9420608295c774
Hanno <chqh@lightahead.cn>
2023-08-02 10:47:04 +0800

    [feat] 新增功能-生成CRC8签名数据

---
+++ tools/sign_check/crc8_sign_check.py
@@ -0,0 +1,26 @@
+"""
+生成CRC8签名信息
+"""
+import binascii
+
+from crcmod import predefined
+
+
+class CRCGenerator(object):
+    def __init__(self):
+        self.module = 'crc-8-maxim'
+
+    def create(self, input_data: str) -> str:
+        crc8 = predefined.Crc(self.module)
+        hex_data = input_data
+        print(hex_data)
+        hex_data = binascii.unhexlify(hex_data)
+        crc8.update(hex_data)
+        result = hex(crc8.crcValue)
+        print(result)
+        return result
+
+
+if __name__ == "__main__":
+    crc = CRCGenerator()
+    crc.create('1E00100463FA0F0219216800220260180524000110001611329355AA')
